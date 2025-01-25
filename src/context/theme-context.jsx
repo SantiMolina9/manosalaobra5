220e03fc712b67c8718665fc5e6b6a44b61ed998
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2025-01-25 19:15:19 -0300

    Creacion de theme-context para manejar dark mode ✨

---
+++ src/context/theme-context.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        document.documentElement.setAttribute("data-theme", newTheme);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            <div className={theme}>{children}</div>
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
