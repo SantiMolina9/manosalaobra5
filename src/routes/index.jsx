8fc4e4e0c0e66e0401ab04d55026bcbbc38c3da3
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2025-01-25 19:32:09 -0300

    Actualizacion de rutas ✨

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,11 +9,16 @@ import { createBrowserRouter } from 'react-router-dom'
 import Tasks from '../features/tasks/index.jsx'
 import ErrorNotFound from '../features/Error/error.jsx'
 import ProtectedRoute from '../features/ProtectedRoute/index.jsx'
+import Register from '../features/register/index.jsx'
 
 export const router = createBrowserRouter([
     {
         path: "/login",
         element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
     },
     {
         path: "/",
